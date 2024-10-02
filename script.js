31f2aae2d9984bd16dcc9e24f3767d241527079a
Warrior <kpanfas6@gmail.com>
2024-10-02 13:57:48 +0530

    Initial Commit

--- script.js
+++
@@ -1,6 +0,0 @@
-var radioButton = document.getElementById("radio-button");
-
-radioButton.addEventListener("click", function() {
-  var audio = new Audio("./Sound_effect.mp3");
-  audio.play();
-});
